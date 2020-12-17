20e21182ad29b0677871074724f063a3825ffc0c
Szymon Kłos <szymon.klos@collabora.com>
2020-12-17 14:44:51 +0100

    lok: don't use focus window to request help
    Use clicked button instead - what leads to the correct window.
    If only focused window was taken into account sometimes
    it caused incorrect help link to be used.
    
    Change-Id: I645dc1ffa6a6d6d363ea5f9874fa830c441516c7
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/107882
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
    Reviewed-by: Szymon Kłos <szymon.klos@collabora.com>
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/108429
    Tested-by: Jenkins

--- vcl/source/app/salvtables.cxx
+++ vcl/source/app/salvtables.cxx
@@ -76,8 +76,7 @@
 #include <window.h>
 #include <wizdlg.hxx>
 #include <salvtables.hxx>
-
-#include <boost/property_tree/ptree.hpp>
+#include <comphelper/lok.hxx>
 
 SalFrame::SalFrame()
     : m_pWindow(nullptr)
@@ -6978,7 +6977,7 @@ void SalInstanceWindow::help()
 {
     //show help for widget with keyboard focus
     vcl::Window* pWidget = ImplGetSVData()->mpWinData->mpFocusWin;
-    if (!pWidget)
+    if (!pWidget || comphelper::LibreOfficeKit::isActive())
         pWidget = m_xWindow;
     OString sHelpId = pWidget->GetHelpId();
     while (sHelpId.isEmpty())
