9834305c308dbdae00d7a041ec69b18c59f37ded
Dennis Francis <dennis.francis@collabora.com>
2021-11-30 10:58:06 +0530

    tdf#144107: fix incorrect conditioning on bGrid...
    introduced in 3092322bb83998397b09d3f34b6d04b5e5c1da50
    
    The intention was to avoid the default line drawing in the original else
    branches to be conditional on bMergeCover. The previous patch used
    'bGrid' as a surrogate for the original 'else' case - which is wrong.
    
    Change-Id: I7c6b49fa9c21064312273b1051ffc7f81de3fef3
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/126077
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
    Reviewed-by: Luboš Luňák <l.lunak@collabora.com>
    Reviewed-by: Dennis Francis <dennis.francis@collabora.com>

--- sc/source/ui/view/output.cxx
+++ sc/source/ui/view/output.cxx
@@ -290,7 +290,7 @@ void ScOutputData::SetSyntaxMode( bool bNewMode )
 void ScOutputData::DrawGrid(vcl::RenderContext& rRenderContext, bool bGrid, bool bPage, bool bMergeCover)
 {
     // bMergeCover : Draw lines in sheet bgcolor to cover lok client grid lines in merged cell areas.
-    // (Used when scNoGridBackground is set in lok mode.)
+    // When scNoGridBackground is set in lok mode, bMergeCover is set to true and bGrid to false.
 
     SCCOL nX;
     SCROW nY;
@@ -354,10 +354,7 @@ void ScOutputData::DrawGrid(vcl::RenderContext& rRenderContext, bool bGrid, bool
     tools::Long nLayoutSign = bLayoutRTL ? -1 : 1;
     tools::Long nSignedOneX = nOneX * nLayoutSign;
 
-    if (bGrid)
-        rRenderContext.SetLineColor(aGridColor);
-    else if (bMergeCover)
-        rRenderContext.SetLineColor(aSheetBGColor);
+    rRenderContext.SetLineColor(bMergeCover ? aSheetBGColor : aGridColor);
 
     ScGridMerger aGrid(&rRenderContext, nOneX, nOneY);
 
@@ -400,14 +397,9 @@ void ScOutputData::DrawGrid(vcl::RenderContext& rRenderContext, bool bGrid, bool
                                                         aPageColor );
                         bDashed = true;
                     }
-                    else if (bGrid)
+                    else
                     {
-                        rRenderContext.SetLineColor( aGridColor );
-                        bDashed = false;
-                    }
-                    else if (bMergeCover)
-                    {
-                        rRenderContext.SetLineColor(aSheetBGColor);
+                        rRenderContext.SetLineColor(bMergeCover ? aSheetBGColor : aGridColor);
                         bDashed = false;
                     }
 
@@ -465,18 +457,14 @@ void ScOutputData::DrawGrid(vcl::RenderContext& rRenderContext, bool bGrid, bool
                             }
                         }
 
-                        if (pThisRowInfo->bChanged && !bHOver && bGrid)
-                        {
-                            aGrid.AddVerLine(bWorksInPixels, nPosX-nSignedOneX, nPosY, nNextY-nOneY, bDashed);
-                        }
-                        else if (bHOver && bMergeCover)
+                        if ((pThisRowInfo->bChanged && !bHOver && !bMergeCover) || (bHOver && bMergeCover))
                         {
                             aGrid.AddVerLine(bWorksInPixels, nPosX-nSignedOneX, nPosY, nNextY-nOneY, bDashed);
                         }
                         nPosY = nNextY;
                     }
                 }
-                else if (bGrid)
+                else if (!bMergeCover)
                 {
                     aGrid.AddVerLine(bWorksInPixels, nPosX-nSignedOneX, nScrY, nScrY+nScrH-nOneY, bDashed);
                 }
@@ -526,14 +514,9 @@ void ScOutputData::DrawGrid(vcl::RenderContext& rRenderContext, bool bGrid, bool
                                                         aPageColor );
                         bDashed = true;
                     }
-                    else if (bGrid)
+                    else
                     {
-                        rRenderContext.SetLineColor( aGridColor );
-                        bDashed = false;
-                    }
-                    else if (bMergeCover)
-                    {
-                        rRenderContext.SetLineColor(aSheetBGColor);
+                        rRenderContext.SetLineColor(bMergeCover ? aSheetBGColor : aGridColor);
                         bDashed = false;
                     }
 
@@ -580,11 +563,7 @@ void ScOutputData::DrawGrid(vcl::RenderContext& rRenderContext, bool bGrid, bool
                                     //! nVisY from Array ??
                             }
 
-                            if (!bVOver && bGrid)
-                            {
-                                aGrid.AddHorLine(bWorksInPixels, nPosX, nNextX-nSignedOneX, nPosY-nOneY, bDashed);
-                            }
-                            else if (bVOver && bMergeCover)
+                            if ((!bVOver && !bMergeCover) || (bVOver && bMergeCover))
                             {
                                 aGrid.AddHorLine(bWorksInPixels, nPosX, nNextX-nSignedOneX, nPosY-nOneY, bDashed);
                             }
@@ -592,7 +571,7 @@ void ScOutputData::DrawGrid(vcl::RenderContext& rRenderContext, bool bGrid, bool
                         nPosX = nNextX;
                     }
                 }
-                else if (bGrid)
+                else if (!bMergeCover)
                 {
                     aGrid.AddHorLine(bWorksInPixels, nScrX, nScrX+nScrW-nOneX, nPosY-nOneY, bDashed);
                 }
