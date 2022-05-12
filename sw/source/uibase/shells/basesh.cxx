LibreOffice/core

sw, out of order undo: allow multiple actions from other views
Previously we assumed that the action to be executed is always exactly
the top of the undo stack minus 1 element.

Extend this, so that in case an other view appends two or more elements
to the undo stack, we still find our undo action. Obviously only do this
if all those undo actions are independent from us.

This requires replacing the swap in svl/ with a move-out + move a range
down + move in construct.

Change-Id: Ic12d32d6eb5e77618d99eddb4fa096802f32d655

sw/source/uibase/shells/basesh.cxx
@@ -627,12 +627,13 @@ void SwBaseShell::ExecUndo(SfxRequest &rReq)
                     const SfxUndoAction* pAction = rManager.GetUndoAction();
                     SwView& rView = rWrtShell.GetView();
                     ViewShellId nViewShellId = rView.GetViewShellId();
+                    sal_uInt16 nOffset = 0;
                     if (pAction->GetViewShellId() != nViewShellId
-                        && rManager.IsViewUndoActionIndependent(&rView))
+                        && rManager.IsViewUndoActionIndependent(&rView, nOffset))
                     {
                         // Execute the undo with an offset: don't undo the top action, but an
                         // earlier one, since it's independent and that belongs to our view.
-                        nUndoOffset = 1;
+                        nUndoOffset += nOffset;
                     }
                 }
 

MODAODF/modaodfsys

(cherry picked from commit 39f231360013e944a8713248359662b9f282d902)
Conflicts:
	sw/source/core/undo/docundo.cxx
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/125102
Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
Reviewed-by: Mike Kaganski <mike.kaganski@collabora.com>

sw/source/uibase/shells/basesh.cxx
@@ -563,12 +563,13 @@ void SwBaseShell::ExecUndo(SfxRequest &rReq)
                     const SfxUndoAction* pAction = rManager.GetUndoAction();
                     SwView& rSwView = rWrtShell.GetView();
                     ViewShellId nViewShellId = rSwView.GetViewShellId();
+                    sal_uInt16 nOffset = 0;
                     if (pAction->GetViewShellId() != nViewShellId
-                        && rManager.IsViewUndoActionIndependent(&rSwView))
+                        && rManager.IsViewUndoActionIndependent(&rSwView, nOffset))
                     {
                         // Execute the undo with an offset: don't undo the top action, but an
                         // earlier one, since it's independent and that belongs to our view.
-                        nUndoOffset = 1;
+                        nUndoOffset += nOffset;
                     }
                 }
 