LibreOffice/core

tdf#143942: oox: import/export labels from <c15:datalabelsRange>
When <c15:showDataLabelsRange> boolean flag is present, the imported
label texts are added as the first text field in oox data label model.
The cell-range associated is also preserved. The export part preserves
the how labels were store originally in <c15:datalabelsRange>.

However in order to make the custom labels reflect the contents of the
cells in the associated cell-range, more work needs to be done. For this
the labels present in <c15:datalabelsRange> needs to be made available
as a data-sequence with a new "role" like "point-labels" in
XInternalDataProvider implementation and and make the label renderer
read this data source rather than consulting the custom label fields
property which is static after import.

Change-Id: Ibc7045fa5ea209d463680c96efb49a06662d2500

oox/source/drawingml/chart/seriesconverter.cxx
@@ -318,9 +318,47 @@ void DataLabelConverter::convertFromModel( const Reference< XDataSeries >& rxDat
             if( nParagraphs > 1 )
                 nSequenceSize += nParagraphs - 1;
 
+            OptValue< OUString > oaLabelText;
+            OptValue< OUString > oaCellRange;
+            if (mrModel.mobShowDataLabelsRange.get(false))
+            {
+                const DataSourceModel* pLabelSource = mrModel.mrParent.mpLabelsSource;
+                if (pLabelSource && pLabelSource->mxDataSeq.is())
+                {
+                    oaCellRange = pLabelSource->mxDataSeq->maFormula;
+                    const auto& rLabelMap = pLabelSource->mxDataSeq->maData;
+                    const auto& rKV = rLabelMap.find(mrModel.mnIndex);
+                    if (rKV != rLabelMap.end())
+                        rKV->second >>= oaLabelText.use();
+                }
+            }
+
+            if (oaLabelText.has())
+                ++nSequenceSize;
+
             aSequence.realloc( nSequenceSize );
 
             int nPos = 0;
+            if (oaLabelText.has())
+            {
+                // Insert a "text" custom label field with the text found in the labels data source.
+                css::uno::Reference< XDataPointCustomLabelField > xCustomLabelText = DataPointCustomLabelField::create( xContext );
+
+                // Store properties
+                oox::PropertySet aPropertySet( xCustomLabelText );
+                convertTextProperty( aPropertySet, getFormatter(), mrModel.mxText->mxTextBody );
+                if (nParagraphs && rParagraphs[0])
+                {
+                    auto& pRuns = rParagraphs[0]->getRuns();
+                    if (pRuns.size())
+                        pRuns[0]->getTextCharacterProperties().pushToPropSet( aPropertySet, getFilter() );
+                }
+                xCustomLabelText->setString(oaLabelText.get());
+                xCustomLabelText->setFieldType( DataPointCustomLabelFieldType::DataPointCustomLabelFieldType_TEXT );
+
+                aSequence[ nPos++ ] = xCustomLabelText;
+            }
+
             for( auto& pParagraph : rParagraphs )
             {
                 for( auto& pRun : pParagraph->getRuns() )
@@ -335,8 +373,17 @@ void DataLabelConverter::convertFromModel( const Reference< XDataSeries >& rxDat
                     TextField* pField = nullptr;
                     if( ( pField = dynamic_cast< TextField* >( pRun.get() ) ) )
                     {
-                        xCustomLabel->setString( pField->getText() );
-                        xCustomLabel->setFieldType( lcl_ConvertFieldNameToFieldEnum( pField->getType() ) );
+                        DataPointCustomLabelFieldType eType = lcl_ConvertFieldNameToFieldEnum( pField->getType() );
+
+                        if (eType == DataPointCustomLabelFieldType::DataPointCustomLabelFieldType_CELLRANGE && oaCellRange.has())
+                        {
+                            xCustomLabel->setString( oaCellRange.get() );
+                            xCustomLabel->setDataLabelsRange( true );
+                        }
+                        else
+                            xCustomLabel->setString( pField->getText() );
+
+                        xCustomLabel->setFieldType( eType );
                         xCustomLabel->setGuid( pField->getUuid() );
                     }
                     else if( pRun )

MODAODF/modaodfsys

oox/source/drawingml/chart/seriesconverter.cxx
@@ -318,9 +318,25 @@ void DataLabelConverter::convertFromModel( const Reference< XDataSeries >& rxDat
             if( nParagraphs > 1 )
                 nSequenceSize += nParagraphs - 1;
 
+            OptValue< OUString > oaLabelText;
+            OptValue< OUString > oaCellRange;
+            if (mrModel.mobShowDataLabelsRange.get(false))
+            {
+                const DataSourceModel* pLabelSource = mrModel.mrParent.mpLabelsSource;
+                if (pLabelSource && pLabelSource->mxDataSeq.is())
+                {
+                    oaCellRange = pLabelSource->mxDataSeq->maFormula;
+                    const auto& rLabelMap = pLabelSource->mxDataSeq->maData;
+                    const auto& rKV = rLabelMap.find(mrModel.mnIndex);
+                    if (rKV != rLabelMap.end())
+                        rKV->second >>= oaLabelText.use();
+                }
+            }
+
             aSequence.realloc( nSequenceSize );
 
             int nPos = 0;
+
             for( auto& pParagraph : rParagraphs )
             {
                 for( auto& pRun : pParagraph->getRuns() )
@@ -335,8 +351,18 @@ void DataLabelConverter::convertFromModel( const Reference< XDataSeries >& rxDat
                     TextField* pField = nullptr;
                     if( ( pField = dynamic_cast< TextField* >( pRun.get() ) ) )
                     {
-                        xCustomLabel->setString( pField->getText() );
-                        xCustomLabel->setFieldType( lcl_ConvertFieldNameToFieldEnum( pField->getType() ) );
+                        DataPointCustomLabelFieldType eType = lcl_ConvertFieldNameToFieldEnum( pField->getType() );
+
+                        if (eType == DataPointCustomLabelFieldType::DataPointCustomLabelFieldType_CELLRANGE && oaCellRange.has())
+                        {
+                            xCustomLabel->setCellRange( oaCellRange.get() );
+                            xCustomLabel->setString( oaLabelText.get() );
+                            xCustomLabel->setDataLabelsRange( true );
+                        }
+                        else
+                            xCustomLabel->setString( pField->getText() );
+
+                        xCustomLabel->setFieldType( eType );
                         xCustomLabel->setGuid( pField->getUuid() );
                     }
                     else if( pRun )